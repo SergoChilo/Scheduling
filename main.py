f7bd2cff56ee5527d8f0e77aba65c917a4dc43ba
SergoChilo <chilingaryan1997@internet.ru>
2022-11-13 13:05:45 +0400

    adding main page ui / empty /

--- main.py
+++ main.py
@@ -1,1 +1,23 @@
+import json
+import os
+import sys
+from PyQt5 import QtWidgets
+from PyQt5.QtWidgets import QApplication, QMainWindow, QListWidget, QAbstractItemView
+from PyQt5.uic import loadUi
 
+class main_window(QtWidgets.QMainWindow):
+    def __init__(self):
+        super().__init__()
+        loadUi("main_page.ui", self)
+
+
+app = QApplication(sys.argv)
+main = main_window()
+widget = QtWidgets.QStackedWidget()
+widget.addWidget(main)
+widget.show()
+
+try:
+    sys.exit(app.exec_())
+except:
+    print("Exiting")
